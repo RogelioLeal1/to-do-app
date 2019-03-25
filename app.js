0e65a3fb2bfa96d33211fcb5a75ed627299a9035
Rogelio Leal I <rogelioleali@eltoro.ws>
2019-03-25 08:44:58 -0400

    Checkpoint 5: Simple To-do List: Complete

--- app.js
+++ app.js
@@ -1,35 +1,67 @@
 function onReady() {
-  const addToDoForm= document.getElementById('addToDoForm');
+  const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit', () => {
+  addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
+
     // get the input text
     let title = newToDoText.value;
 
-    // create new li
+    // create a new li
     let newLi = document.createElement('li');
 
     // create a new input
     let checkbox = document.createElement('input');
 
-    //  set the input's type to checkbox
+    // create delete button
+    let deleteButton = document.createElement('button');
+
+    // set the input's type to checkbox
     checkbox.type = "checkbox";
 
     // set the title
     newLi.textContent = title;
 
-    // attach the checkbox to Li
-    newLi.appendChild(checkbox);
+    // set the button text
+    deleteButton.innerHTML = "<i class='fa fa-trash-o de'></i>";
+
+    // newLi style
+ newLi.classList.add("demo-card-wide", "mdl-shadow--2dp");
+
+    // newLi style *
+   newLi.classList.add("mdl-list__item-primary-content");
+
+    // deleteButton styles
+   deleteButton.classList.add("mdl-button", "mdl-js-button", "mdl-button--fab", "mdl-button--mini-fab");
+
+    // attach the checkbox to the li
+    newLi.prepend(checkbox);
+
+    // attach the button to the li
+    newLi.appendChild(deleteButton);
 
     // attach the li to the ul
     toDoList.appendChild(newLi);
 
-   //empty the input
-   newToDoText.value = ' ';
+    // empty the input
+    newToDoText.value = '';
+
+
+
+    // delete
+    deleteButton.onclick = function () {
+      toDoList.removeChild(newLi);
+    }
+
   });
 }
+
+window.onload = function() {
+  onReady();
+};
+
 
 window.onload = function() {
 onReady();
